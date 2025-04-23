d8ad237956cad390066de5ea03554eac30596e56
Toothless <Kakani2789@gmail.com>
2025-04-23 10:10:55 -0400

    Update context.go

--- cmd/api/context.go
+++ cmd/api/context.go
@@ -14,13 +14,11 @@ func (app *application) contextSetUser(r *http.Request, user *data_1.User) *http
 	ctx := context.WithValue(r.Context(), userContextKey, user)
 	return r.WithContext(ctx)
 }
-
 func (app *application) contextGetUser(r *http.Request) *data_1.User {
 	user, ok := r.Context().Value(userContextKey).(*data_1.User)
 
 	if !ok {
 		panic("missing user value in request context")
 	}
-
 	return user
 }
